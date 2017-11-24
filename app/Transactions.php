83c5f9808a2dcac9381fbfe7e6c70a9d455c2035
Venipa <admin@venipa.net>
2017-11-24 13:27:49 +0100

    SRR Fix

--- app/Transactions.php
+++ app/Transactions.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transactions extends Model
 {
-    //
+    protected $connection = 'mssql_web';
+    protected $primaryKey = 'id';
+    protected $table = 'srr_transactions';
+    protected $guarded = ['id'];
+
+    const CREATED_AT = null;
+    const UPDATED_AT = null;
 }
